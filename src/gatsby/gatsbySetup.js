8d85755f71646b7388550b04c13cd1396aa60be9
Karol Grochowalski <karol.grochowalski@gmail.com>
2020-05-08 09:30:44 +0200

    Zlecenie #19108 - CLI do budowania starterów

--- src/gatsby/gatsbySetup.js
+++ src/gatsby/gatsbySetup.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').exec;
 const config = require('../configuration/config');
 const fs = require('fs');
+const path = require('path');
 
 exports.setup = async (projectDirectory, starterUrl) => {
     console.log('Gatsby setup');
@@ -8,43 +9,29 @@ exports.setup = async (projectDirectory, starterUrl) => {
 }
 
 exports.init = async (projectDirectory, apiKey) => {
-    let fileContent = 'GATSBY_FLOTIQ_BASE_URL='+ config.apiUrl+'\n' +
-        'FLOTIQ_API_KEY=' + apiKey+ '\n' +
-        'SNIPCART_API_KEY=YOUR SNIPCART PUBLIC API KEY'
+    let fileContent = 'GATSBY_FLOTIQ_BASE_URL=' + config.apiUrl + '\n' +
+        'FLOTIQ_API_KEY=' + apiKey + '\n';
     fs.writeFile(projectDirectory + '/.env', fileContent, (err) => {
         if (err) throw err;
-        console.log('Configuration is created successfully: '  + projectDirectory + '/.env')
+        console.log('Configuration is created successfully: ' + projectDirectory + '/.env')
     });
 }
 
 exports.develop = async (projectDirectory) => {
-    // await execShellCommand('cd ' + projectDirectory + ' && npm install');
     await execShellCommand('cd ' + projectDirectory + ' && ' + createGatsbyCommand('develop'));
 }
 
 function runGatsbyProcess(action, projectDirectory = '', starterUrl = '') {
-
-    let cmd = __dirname +'/../../'+ config.gatsbyCli;
-    if (process.platform === 'win32') {
-
-        cmd = cmd.replace("/.//gi", "");
-        cmd = cmd.replace("///gi", "\\");
-    }
-
-    cmd = cmd + ' ' + action + ' ' + projectDirectory + ' ' + starterUrl;
+    let cmd = createGatsbyCommand(action, projectDirectory, starterUrl);
     return execShellCommand(cmd);
 }
 
 function createGatsbyCommand(action, projectDirectory = '', starterUrl = '') {
-    let cmd = __dirname +'/../../'+ config.gatsbyCli;
-    if (process.platform === 'win32') {
-
-        cmd = cmd.replace("/.//gi", "");
-        cmd = cmd.replace("///gi", "\\");
-    }
+    let cmd = path.resolve(__dirname, '..', '..', config.gatsbyCli);
 
     return cmd + ' ' + action + ' ' + projectDirectory + ' ' + starterUrl;
 }
+
 function execShellCommand(cmd) {
     return new Promise((resolve, reject) => {
         let commandProcess = exec(cmd, (error, stdout, stderr) => {
@@ -54,7 +41,7 @@ function execShellCommand(cmd) {
             }
             resolve(stdout ? stdout : stderr);
         });
-        // live output from gatsby
+        // live output from command
         commandProcess.stdout.on('data', function (data) {
             console.log('\x1b[36m%s\x1b[0m', data);
         });
