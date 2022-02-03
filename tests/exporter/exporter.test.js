ff8db4c54feb26b43e697b1d966b5c0eef91981b
Karol Grochowalski <karol.grochowalski@gmail.com>
2022-02-03 22:19:59 +0100

    exporter tests refactor

--- tests/exporter/exporter.test.js
+++ tests/exporter/exporter.test.js
@@ -1,22 +1,11 @@
 const exporter = require('../../src/exporter/exporter');
-const {when, resetAllWhenMocks, verifyAllWhenMocksCalled} = require("jest-when");
+const {when, resetAllWhenMocks, verifyAllWhenMocksCalled} = require('jest-when');
+const assert = require('assert');
+const config = require('../../src/configuration/config');
+
 jest.mock('node-fetch');
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 const {Response} = jest.requireActual('node-fetch');
-const config = require("../../src/configuration/config");
-
-const ctd1List = require('./mocks/ctds-page1');
-const CTDS1 = JSON.stringify(ctd1List.ctds);
-
-const ctd2List = require('./mocks/ctds-page1');
-const CTDS2 = JSON.stringify(ctd2List.ctds);
-
-const coList1 = require('./mocks/ctos1-page-1');
-const CTOS1 = JSON.stringify(coList1.ctds);
-
-const coList2 = require('./mocks/ctos1-page-2');
-const assert = require("assert");
-const CTOS2 = JSON.stringify(coList2.ctds);
 
 beforeEach(() => {
     resetAllWhenMocks()
@@ -27,47 +16,12 @@ describe('Exporter test', () => {
         let apiKey = 'test-apy-key';
         let directoryPath = 'path';
 
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/internal/contenttype?auth_token=${apiKey}&internal=0&page=1&limit=100`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTDS1)));
-
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/content/Type-1-name?auth_token=${apiKey}&page=1&limit=10`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTOS1)));
-
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/content/Type-1-name?auth_token=${apiKey}&page=2&limit=10`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTOS2)));
-
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/internal/contenttype?auth_token=${apiKey}&internal=0&page=2&limit=100`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTDS2)));
-
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/content/Type-1-name?auth_token=${apiKey}&page=1&limit=10`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTOS1)));
-
-        when(fetch)
-            .expectCalledWith(
-                expect.stringContaining(`${config.apiUrl}/api/v1/content/Type-1-name?auth_token=${apiKey}&page=2&limit=10`),
-                {method: 'GET'}
-            )
-            .mockReturnValueOnce(Promise.resolve(new Response(CTOS2)));
+        mockContentTypesDefinition(apiKey, 1);
+        mockContentObjects(apiKey, 'Type-1-name', 1);
+        mockContentObjects(apiKey, 'Type-1-name', 2);
+        mockContentTypesDefinition(apiKey, 2);
+        mockContentObjects(apiKey, 'Type-2-name', 1);
+        mockContentObjects(apiKey, 'Type-2-name', 2);
 
         let result = await exporter.export(apiKey, directoryPath);
 
@@ -77,3 +31,31 @@ describe('Exporter test', () => {
         assert.equal(40, result.totalObjects);
     })
 })
+
+const mockContentTypesDefinition = (apiKey, page) => {
+    let ctdList = require('./mocks/ctds-page2');
+    if (page === 1) {
+        ctdList = require('./mocks/ctds-page1');
+    }
+    const CTDS = JSON.stringify(ctdList.ctds);
+    when(fetch)
+        .expectCalledWith(
+            expect.stringContaining(`${config.apiUrl}/api/v1/internal/contenttype?auth_token=${apiKey}&internal=0&page=${page}&limit=100`),
+            {method: 'GET'}
+        )
+        .mockReturnValueOnce(Promise.resolve(new Response(CTDS)));
+}
+
+const mockContentObjects = (apiKey, type, page) => {
+    let coList = require('./mocks/ctos1-page-2');
+    if (page === 1) {
+        coList = require('./mocks/ctos1-page-1');
+    }
+    const CTOS1 = JSON.stringify(coList.ctds);
+    when(fetch)
+        .expectCalledWith(
+            expect.stringContaining(`${config.apiUrl}/api/v1/content/${type}?auth_token=${apiKey}&page=${page}&limit=10`),
+            {method: 'GET'}
+        )
+        .mockReturnValueOnce(Promise.resolve(new Response(CTOS1)));
+}
