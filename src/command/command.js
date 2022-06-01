d99f3dc7c09c80fa3fc404ec91b0d84a66cdc53e
veto221 <=>
2022-06-01 09:24:22 +0200

    change command to not show questions about flotiq api key if there is one defined in env

--- src/command/command.js
+++ src/command/command.js
@@ -18,8 +18,7 @@ yargs
     .boolean('json-output')
     .alias('json-output', ['j'])
     .describe('json-output', ' Whether to save results as JSON')
-    .command('start [flotiqApiKey] [directory] [url]', 'Start the project', (yargs) => {
-        optionalParamFlotiqApiKey(yargs);
+    .command('start [directory] [url] [flotiqApiKey]', 'Start the project', (yargs) => {
         yargs.positional('directory', {
             describe: 'Directory to create project in.',
             type: 'string',
@@ -28,68 +27,65 @@ yargs
             describe: 'Url to git repository with Gatbsy starter.',
             type: 'string',
         });
+        optionalParamFlotiqApiKey(yargs);
     }, async (argv) => {
         console = custom.console(oldConsole, yargs.argv['json-output'], errors, stdOut, errorObject, fs);
         if (yargs.argv.help) {
             yargs.showHelp();
             process.exit(1);
         }
-        if (yargs.argv._.length < 4) {
+        if (yargs.argv._.length < 3) {
             let answers = await askQuestions(questionsText.START_QUESTIONS);
             let {flotiqApiKey, projectDirectory, url} = answers;
-            if (flotiqApiKey === '' || flotiqApiKey === null) {
-                flotiqApiKey = process.env.FLOTIQ_API_KEY;
-            }
-            start(flotiqApiKey, projectDirectory, url, yargs.argv['json-output'])
-
+            start(flotiqApiKey, projectDirectory, url, yargs.argv['json-output']);
+        } else if (yargs.argv._.length === 3 && process.env.FLOTIQ_API_KEY !== "") {
+            start(process.env.FLOTIQ_API_KEY, argv.directory, argv.url, yargs.argv['json-output']);
         } else if (yargs.argv._.length === 4) {
-            start(argv.flotiqApiKey, argv.directory, argv.url, yargs.argv['json-output'])
+            start(argv.flotiqApiKey, argv.directory, argv.url, yargs.argv['json-output']);
         } else {
             yargs.showHelp();
             process.exit(1);
         }
     })
-    .command('import [flotiqApiKey] [directory]', 'Import objects from directory to Flotiq', (yargs) => {
-        optionalParamFlotiqApiKey(yargs);
+    .command('import [directory] [flotiqApiKey]', 'Import objects from directory to Flotiq', (yargs) => {
         yargs.positional('directory', {
             describe: 'Directory path with Flotiq sample data (directory cannot be empty, if you wish to run command in current directory, insert . (dot)).',
             type: 'string',
         });
+        optionalParamFlotiqApiKey(yargs);
     }, async (argv) => {
         console = custom.console(oldConsole, yargs.argv['json-output'], errors, stdOut, errorObject, fs);
-        if (yargs.argv._.length < 3) {
+        if (yargs.argv._.length < 2) {
             const answers = await askQuestions(questionsText.IMPORT_QUESTIONS);
             let {flotiqApiKey, projectDirectory} = answers;
-            if (flotiqApiKey === '' || flotiqApiKey === null) {
-                flotiqApiKey = process.env.FLOTIQ_API_KEY;
-            }
             let directory = getObjectDataPath(projectDirectory);
             await importer.importer(flotiqApiKey, directory, true);
+        } else if (yargs.argv._.length === 2 && process.env.FLOTIQ_API_KEY !== "") {
+            let directory = getObjectDataPath(argv.directory);
+            await importer.importer(process.env.FLOTIQ_API_KEY, directory, true);
         } else if (yargs.argv._.length === 3) {
             let directory = getObjectDataPath(argv.directory);
             await importer.importer(argv.flotiqApiKey, directory, true);
         }
     })
-    .command('wordpress-import [flotiqApiKey] [wordpressUrl]', 'Import wordpress to Flotiq', (yargs) => {
-        optionalParamFlotiqApiKey(yargs);
+    .command('wordpress-import [wordpressUrl] [flotiqApiKey]', 'Import wordpress to Flotiq', (yargs) => {
         yargs.positional('wordpressUrl', {
             describe: 'Url to wordpress blog project',
             type: 'string',
         });
+        optionalParamFlotiqApiKey(yargs);
     }, async (argv) => {
 
         const wordpressStart = require('flotiq-wordpress-import').start;
         // overriding the console in this case is not required, custom console is build in wordpress-importer
-        if (yargs.argv._.length < 3) {
+        if (yargs.argv._.length < 2) {
             const answers = await askQuestions(questionsText.WORDPRESS_IMPORT_QUESTIONS);
             let {flotiqApiKey, wordpressUrl} = answers;
-            if (flotiqApiKey === '' || flotiqApiKey === null) {
-                flotiqApiKey = process.env.FLOTIQ_API_KEY;
-            }
             wordpressStart(flotiqApiKey, wordpressUrl, yargs.argv['json-output'])
-
+        } else if (yargs.argv._.length === 2 && process.env.FLOTIQ_API_KEY !== "") {
+            wordpressStart(process.env.FLOTIQ_API_KEY, argv.wordpressUrl, yargs.argv['json-input']);
         } else if (yargs.argv._.length === 3) {
-            wordpressStart(argv.flotiqApiKey, argv.wordpressUrl, yargs.argv['json-output'])
+            wordpressStart(argv.flotiqApiKey, argv.wordpressUrl, yargs.argv['json-output']);
         }
     })
     .command(
@@ -115,23 +111,22 @@ yargs
             }
         })
     .command(
-        'export [flotiqApiKey] [directory]',
+        'export [directory] [flotiqApiKey]',
         'Export objects from Flotiq to directory',
         (yargs) => {
-            optionalParamFlotiqApiKey(yargs);
             yargs.positional('directory', {
                 describe: 'Directory path to save data.',
                 type: 'string',
             });
+            optionalParamFlotiqApiKey(yargs);
         }, async (argv) => {
             console = custom.console(oldConsole, yargs.argv['json-output'], errors, stdOut, errorObject, fs);
-            if (yargs.argv._.length < 3) {
+            if (yargs.argv._.length < 2) {
                 const answers = await askQuestions(questionsText.EXPORT_QUESTIONS);
                 let {flotiqApiKey, projectDirectory} = answers;
-                if (flotiqApiKey === '' || flotiqApiKey === null) {
-                    flotiqApiKey = process.env.FLOTIQ_API_KEY;
-                }
                 await exporter.export(flotiqApiKey, projectDirectory, true);
+            } else if (yargs.argv._.length === 2 && process.env.FLOTIQ_API_KEY !== "") {
+                await exporter.export(process.env.FLOTIQ_API_KEY, argv.directory, true);
             } else if (yargs.argv._.length === 3) {
                 await exporter.export(argv.flotiqApiKey, argv.directory, true);
             }
@@ -142,7 +137,7 @@ yargs
 checkCommand(yargs, 0);
 
 function optionalParamFlotiqApiKey(yargs) {
-    if (process.env.FLOTIQ_API_KEY !== "") {
+    if (process.env.FLOTIQ_API_KEY === "") {
         yargs.positional('flotiqApiKey', {
             describe: 'Flotiq Read and write API KEY.',
             type: 'string',
