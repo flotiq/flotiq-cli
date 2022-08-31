f543ed0a7ef4057b51b0f0cc82d020f220397331
WHLukasz <lukasz.wach@codewave.com>
2022-08-31 13:13:42 +0200

    version up

--- src/temp/flotiq-contentful-import.js
+++ src/temp/flotiq-contentful-import.js
@@ -44,11 +44,11 @@ module.exports = contentful = async (flotiq_ApiKey, cont_spaceId, cont_contentMa
         return;
     }
 
-    resultMedia = await importMedia(exportData.assets, translation, flotiq_ApiKey, cont_spaceId); 
+    // resultMedia = await importMedia(exportData.assets, translation, flotiq_ApiKey, cont_spaceId); 
 
     await importCtd(exportData.contentTypes);
 
-    importCo(exportData.entries, resultMedia, translation);
+    // importCo(exportData.entries, resultMedia, translation);
 }
 
 async function importCtd(data) {
@@ -73,131 +73,149 @@ async function importCtd(data) {
                 propertiesConfig: {},
             },
         };
+        
+        // (todo) appropriate message for change of the ctd name, this one i wouldnt call appropriate
+        if (/\d/.test(ctdRec.name)) {
+            console.log("\n\n", ctdRec.name, " has a number!\n\n");   
+        }
 
         obj.fields.forEach((field) => {
-            
-            //console.log('test ctd field: ', field); // DEL
+
             if (field.required === true) {
                 ctdRec.schemaDefinition.required.push(field.id);
             }
             ctdRec.metaDefinition.order.push(field.id);
 
-            ctdRec.schemaDefinition.allOf[1].properties[field.id] = {
-                type: findJsonType(field.type), // (?) idk how else should I do this. CF doesn't enlist param type in JSON. Not even sure if it is what i think it is
-                minLength: 1,
-            };
-
-            if (ctdRec.schemaDefinition.allOf[1].properties[field.id].type === "array") {
-                ctdRec.schemaDefinition.allOf[1].properties[field.id].items = {
-                    $ref: "#/components/schemas/DataSource",
-                    type: "test" //convertFieldType(field.type, field.linkType),
-                }
-            }
-
-            ctdRec.metaDefinition.propertiesConfig[field.id] = {
-                label: field.name,
-                unique: false,
-                helpText: "", // (?) haven't found any kind of helptext prop in CF fields
-                inputType: convertFieldType(field.type, field.linkType), // (todo) make CF field types match Flotiq field types
-                //CF FT: Rich text, Text, Number, Date, Location, Media, Boolean, JSON, Reference
-                //Flotiq FT: Text, Textarea, Markdown, Rich text, Email, Number, Radio, Checkbox, Select, Relation, List, Geo, Media, Date, Block
-                
-                // (todo) other properties like :isTitlePart
-            };
-            field.validations.forEach((validation) => {
-                if (!!validation.unique) {
-                    ctdRec.metaDefinition.propertiesConfig[field.id].unique = true;
-                }
-            });
-
-            if (ctdRec.metaDefinition.propertiesConfig[field.id].inputType === "datasource") {
-                ctdRec.metaDefinition.propertiesConfig[field.id].validation = {};
-                let count = 0;
-                
-                // for (prop in field.validations) { // (todo) (?) count validations in flotiq ctdRec instead of cf's ctd. idk if i get relationMultiple right
-                //     if (field.validations.hasOwnProperty(prop)) {
-                //         count++;
-                //     }
-                // }
-
-                // if (count > 0) {
-                //     ctdRec.metaDefinition.propertiesConfig[field.id].validation.relationMultiple = true; // (TODO) needs testing
-                // }
-
-                if (field.linkType === "Asset") {
-                    ctdRec.metaDefinition.propertiesConfig[field.id].validation.relationContenttype = "_media";
-                }
-                
-                if (field.linkType === "Entry") { // (?) not sure if link to ctd should be only for `Entry` linkType
-                    field.validations.forEach((prop) => {
-                        if (prop.hasOwnProperty("linkContentType")) {
-                            ctdRec.metaDefinition.propertiesConfig[field.id].validation.relationContenttype = prop.linkContentType[0]; // (TODO) (?) how to make relation to multiple ctds in flotiq api?
-                            if (prop.linkContentType.length > 1) {
-                                ctdRec.metaDefinition.propertiesConfig[field.id].validation.relationMultiple = true; // (TODO) take care of todo above, then fix this
-                            }
-                        }
-                    });
-                }
-
-                ctdRec.schemaDefinition.allOf[1].properties[field.id].items = {
-                    $ref: "#/components/schemas/DataSource",
-                }
-                
-                if (field.type === "Array") {
-                    ctdRec.schemaDefinition.allOf[1].properties[field.id].items.type = findJsonType(field.items.type);
-                    ctdRec.schemaDefinition.allOf[1].properties[field.id].items.validation = field.items.validations[0];
-                }
-            }
-
+            ctdRec.schemaDefinition.allOf[1].properties[field.id] = buildSchemaDefinition(field);
+            ctdRec.metaDefinition.propertiesConfig[field.id] = buildMetaDefinition(field);
         });
 
-            //  IMPORT
+        //  IMPORT
         let result = await fetch(config.apiUrl + '/api/v1/internal/contenttype', {
             method: 'POST',
             body: JSON.stringify(ctdRec), // (?) error code 500 Internal server error
             headers: {...headers, 'Content-Type': 'application/json'},
         })
+        console.log("Import: ", ctdRec.name); // DEL
         console.log(await result.json());
-        console.log('test import json: ', JSON.stringify(ctdRec, null, 2)); // DEL
-    })
-}
+        // console.log(JSON.stringify(ctdRec, null, 2)); // DEL
+    });
 
-function findJsonType(type) {
-    // 
-    if (type === "Text" || type === "Symbol" || type === "Date") return ("string");
-    if (type === "Integer" || type === "Number") return ("number");
-    if (type === "Location" || type === "Object") return ("object");
-    if (type === "Array" || type === "Link") return ("array");
-    if (type === "Boolean") return ("boolean");
-    
-    return ("Unknown field type");
-    // (todo) change to object and return its proper value like in convertFieldType below
-}
 
-function convertFieldType(type, linkType) {
-    // if (type === "Link") {
-    //     if (linkType === "Asset") return ("datasource");
-    //     if (linkType === "Entry") return ("datasource"); // (?) TBU idk what field type should it be
-    //     console.error("Unknown link type!");
-    // }
-    // if (type === "Array") {
-    //     return ("datasource");
-    // } //DEL?
-    objTypes = {
-        RichText: "richtext",
-        Text: "text",
-        Symbol: "text",
-        Integer: "number",
-        Number: "number",
-        Date: "dateTime",
-        Location: "geo",
-        Array: "datasource", // (?) TBU
-        Boolean: "checkbox", // (?)
-        Object: "object",
-        Link: "datasource", // (?)
+    function buildSchemaDefinition(field) {
+        
+        let schemaDefinition = {
+            type: findJsonType(field.type)
+        };
+
+        if (field.type === "Link" || field.type === "Array" && field.items.type === "Link" && field.items.linkType === "Entry") {
+            schemaDefinition.items = {
+                $ref: "#/components/schemas/DataSource",
+            }
+            schemaDefinition.minItems = 1;
+        } else if (field.type === "Array") {
+            // (TODO) find if CF can have nested fields in array/field/object
+            schemaDefinition.items = buildSchemaDefinition(field.items);
+            schemaDefinition.minItems = 1;
+        } else if (field.type === "Object") {
+            // schemaDefinition.properties = buildSchemaDefinition(field.items); // (TODO) check if works with json objects properly
+            schemaDefinition.minItems = 1;
+        } else {
+            schemaDefinition.minLength = 1;
+        }
+
+        return schemaDefinition;
     }
-    return objTypes[type];
+    
+    function buildMetaDefinition(field) {
+        let metaDefinition = {
+            label: field.name,
+            unique: false,
+            helpText: "", // (todo) get helptext prop from CF fields. where?
+            inputType: convertFieldType(field.type, field.linkType), // (todo) make CF field types match Flotiq field types
+            //CF FT: Rich text, Text, Number, Date, Location, Media, Boolean, JSON, Reference
+            //Flotiq FT: Text, Textarea, Markdown, Rich text, Email, Number, Radio, Checkbox, Select, Relation, List, Geo, Media, Date, Block
+            
+            // (todo) other properties like :isTitlePart
+        }
+
+        field.validations.forEach((validation) => {
+            if (!!validation.unique) {
+                metaDefinition.unique = true;
+            }
+        });
+
+        if (metaDefinition.inputType === "datasource") {
+            metaDefinition.validation = {};
+
+            if (field.linkType === "Asset") {
+                metaDefinition.validation.relationContenttype = "_media";
+            }
+            
+            if (field.linkType === "Entry") {
+                metaDefinition.validation.relationMultiple = true; // (?) (todo) how to determine if multiple relation should be enabled? for now it is by default
+                metaDefinition.validation.relationContenttype = field.validations[0].linkContentType[0]
+            }
+        }
+        if (field.type === "Array") {
+            if (field.items.type === "Link" && field.items.linkType === "Entry") {
+                metaDefinition.validation.relationMultiple = true; // (?) (todo) how to determine if multiple relation should be enabled? for now it is by default
+                metaDefinition.validation.relationContenttype = "";
+                // (?) does flotiq accept multiple ctd relations?
+
+            } else {
+                // THIS DOESNT WORK YET!  (TODO) find if CF can have nested fields in array/field/object
+                metaDefinition.items = {};
+                metaDefinition.items.order = [];
+                metaDefinition.items.propertiesConfig = buildMetaDefinition(field.items);
+            }
+        }
+        if (field.type === "Object") {
+            // (TODO) json object, should look similar to array above
+        }
+
+        return metaDefinition;
+    }
+
+    function findJsonType(type) {
+        // 
+        if (type === "Text" || type === "Symbol" || type === "Date") return ("string");
+        if (type === "Integer" || type === "Number") return ("number");
+        if (type === "Location" || type === "Object") return ("object");
+        if (type === "Array" || type === "Link") return ("array");
+        if (type === "Boolean") return ("boolean");
+        
+        return ("Unknown field type");
+        // (todo) change to object and return its proper value like in convertFieldType below
+    }
+    
+    function convertFieldType(type, linkType) {
+        // if (type === "Link") {
+        //     if (linkType === "Asset") return ("datasource");
+        //     if (linkType === "Entry") return ("datasource"); // (?) TBU idk what field type should it be
+        //     console.error("Unknown link type!");
+        // }
+        // if (type === "Array") {
+        //     return ("datasource");
+        // } //DEL?
+        objTypes = {
+            RichText: "richtext",
+            Text: "text",
+            Symbol: "text",
+            Integer: "number",
+            Number: "number",
+            Date: "dateTime",
+            Location: "geo",
+            Array: "datasource", // (?) TBU
+            Boolean: "checkbox", // (?)
+            Object: "object",
+            Link: "datasource", // (?)
+        }
+        return objTypes[type];
+    }
 }
+
+
 
 async function importCo(data, media, trans) {
     
@@ -211,7 +229,6 @@ async function importCo(data, media, trans) {
             if (field.hasOwnProperty("sys") == false) {
                 coRec[i] = field;
             } else if (field.sys.type === "Link") {
-
                 media.forEach((asset) => {
                     if (field.sys.id === asset.id) {
                         coRec[i] = [{
