50573ed54ca67a1efc4144ad11b3a394d48b6200
WHLukasz <109143307+WHLukasz@users.noreply.github.com>
2023-04-06 13:09:46 +0200

    Feature/22670-import-batch-limit (#77)
    * add batch limit flag
    
    * add missing updateExisting flag
    
    * update README
    
    * update package.json
    
    ---------
    
    Co-authored-by: WHLukasz <lukasz.wach@codewave.com>

--- src/command/command.js
+++ src/command/command.js
@@ -277,6 +277,12 @@ yargs
                 .number('limit')
                 .alias('limit', ['l'])
                 .describe('number of Content Objects imported counting from the top row, default: 10.000')
+                .number('batchLimit')
+                .alias('batchLimit', ['bl'])
+                .describe('batchLimit', 'number of Content Objects imported per batch call, default: 100')
+                .boolean('updateExisting')
+                .alias('updateExisting', ['ue'])
+                .describe('If content objects with a given id already exist in the Flotiq account, they will be updated')
         }, async (argv) => {
             if (yargs.argv._.length < 3 || (yargs.argv._.length === 3 && !apiKeyDefinedInDotEnv())) {
                 const answers = await askQuestions(questionsText.EXCEL_MIGRATION);
@@ -286,7 +292,9 @@ yargs
                     ctdName: ctdName,
                     filePath: filePath,
                     limit: yargs.argv['limit'],
-                    logResults: !yargs.argv['hideResults']
+                    logResults: !yargs.argv['hideResults'],
+                    batchLimit: yargs.argv['batchLimit'],
+                    updateExisting: yargs.argv['updateExisting']
                 });
             } else if (yargs.argv._.length <= 4) {
                 if (!argv.flotiqApiKey && apiKeyDefinedInDotEnv()) {
@@ -297,7 +305,9 @@ yargs
                     ctdName: argv.ctdName,
                     filePath: argv.filePath,
                     limit: yargs.argv['limit'],
-                    logResults: !yargs.argv['hideResults']
+                    logResults: !yargs.argv['hideResults'],
+                    batchLimit: yargs.argv['batchLimit'],
+                    updateExisting: yargs.argv['updateExisting']
                 });
             } else {
                 yargs.showHelp();
